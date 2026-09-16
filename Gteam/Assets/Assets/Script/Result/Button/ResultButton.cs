2fff8b2d17827a94fa517eeb3e434d7384fb1e83
Anko0614 <stakuya0614d@gmail.com>
2026-09-17 02:14:55 +0900

    ResultSceneの大まかな形作成

---
+++ Gteam/Assets/Assets/Script/Result/Button/ResultButton.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResultButton : MonoBehaviour
+{
+    public void RestartBtn()
+    {
+        SceneManager.LoadScene("GameScene1");
+    }
+
+    public void ExitBtn()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
