a805dd3180c12509d85f28e8800ff8d7df180eb5
1cat2525 <894z9f9642@privaterelay.appleid.com>
2026-09-18 16:09:06 +0900

    ゲームクリア、ゲームオーバーの調整

---
+++ Gteam/Assets/Assets/Script/Game2/UI/FadeManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeManager : MonoBehaviour
+{
+    [SerializeField]
+    private Image fadeImage;
+
+    public IEnumerator FadeOut(float duration)
+    {
+        float timer = 0f;
+
+        Color color = fadeImage.color;
+
+        while (timer < duration)
+        {
+            timer += Time.deltaTime;
+
+            color.a = timer / duration;
+
+            fadeImage.color = color;
+
+            yield return null;
+        }
+
+        color.a = 1f;
+        fadeImage.color = color;
+    }
+}
