977646ac2e060a9f44e753b36117f7ef0f9b938c
1cat2525 <894z9f9642@privaterelay.appleid.com>
2026-09-18 23:32:43 +0900

    シーン変更の追加？

---
+++ Gteam/Assets/Assets/Script/Title/BGM.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMManager : MonoBehaviour
+{
+    private static BGMManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
