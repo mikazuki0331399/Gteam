6e207d26173b0e118c7a80a25576c0b2c3fd2bb1
1cat2525 <894z9f9642@privaterelay.appleid.com>
2026-09-19 16:22:17 +0900

    Game2の復旧

---
+++ Gteam/Assets/Assets/Script/Game2/UI/GameStartController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStartController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject tutorialPanel;
+
+    [SerializeField]
+    private PlayerPowerController playerController;
+
+    [SerializeField]
+    private TimerController timerController;
+
+    [SerializeField]
+    private TargetRangeController targetRangeController;
+
+    [SerializeField]
+    private TroubleManager troubleManager;
+
+    [SerializeField]
+    private SystemErrorManager systemErrorManager;
+
+    private bool gameStarted = false;
+
+    void Start()
+    {
+        playerController.enabled = false;
+        timerController.enabled = false;
+        targetRangeController.enabled = false;
+        troubleManager.enabled = false;
+        systemErrorManager.enabled = false;
+    }
+
+    void Update()
+    {
+        if (!gameStarted && Input.anyKeyDown)
+        {
+            gameStarted = true;
+
+            tutorialPanel.SetActive(false);
+
+            playerController.enabled = true;
+            timerController.enabled = true;
+            targetRangeController.enabled = true;
+            troubleManager.enabled = true;
+            systemErrorManager.enabled = true;
+        }
+    }
+}
